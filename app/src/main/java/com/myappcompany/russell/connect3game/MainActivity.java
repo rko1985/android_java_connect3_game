8ce6fda193a4032ada9acc63d1e957eb8cada1d1
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-07 11:47:11 -1000

    dropIn function created

--- app/src/main/java/com/myappcompany/russell/connect3game/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/connect3game/MainActivity.java
@@ -2,8 +2,19 @@ package com.myappcompany.russell.connect3game;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void dropIn(View view){
+
+        ImageView counter = (ImageView) view; //finding the piece that is going to drop from view
+        counter.setTranslationY(-1500); //move off the screen
+        counter.setImageResource(R.drawable.yellow); //set image to yellow counter
+        counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
