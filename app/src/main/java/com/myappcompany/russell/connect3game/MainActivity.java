4442a565cf6a95ecfd8bf54012734e439b98e351
rko1985 <35703264+rko1985@users.noreply.github.com>
2020-02-07 11:52:22 -1000

    alternates between yellow and red

--- app/src/main/java/com/myappcompany/russell/connect3game/MainActivity.java
+++ app/src/main/java/com/myappcompany/russell/connect3game/MainActivity.java
@@ -7,11 +7,20 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int activePlayer = 0; //0 for yellow 1 for red
+
     public void dropIn(View view){
 
         ImageView counter = (ImageView) view; //finding the piece that is going to drop from view
         counter.setTranslationY(-1500); //move off the screen
-        counter.setImageResource(R.drawable.yellow); //set image to yellow counter
+        if(activePlayer == 0) {
+            counter.setImageResource(R.drawable.yellow); //set image to yellow counter
+            activePlayer = 1; // set to red
+        } else {
+            counter.setImageResource(R.drawable.red); //set imag
+            activePlayer = 0;
+        }
+
         counter.animate().translationYBy(1500).rotation(3600).setDuration(300);
 
     }
